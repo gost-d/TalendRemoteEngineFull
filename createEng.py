3dc8fd90d23601932326f018272e9f85d0d2464c
Dawid Gostyński <daw.gostynski@gmail.com>
2022-11-26 11:08:38 +0100

    Add token

--- createEng.py
+++ createEng.py
@@ -5,6 +5,11 @@ import json
 from datetime import datetime
 
 
+pToken = "Bearer wHiwDdZWTTasSdvCFIxNmwcSXglP2ozKgnU7qdpKXP1-Sh1z3ygwyJcNkVF6z4H5"
+talendWorkspaces = "https://api.eu.cloud.talend.com/orchestration/workspaces"
+headers = {'Authorization': pToken}
+
+
 def getWorkSpaceAndEnvIds(env="default", personalToken = pToken):
     """
     Function to get workspace and environment id`s
