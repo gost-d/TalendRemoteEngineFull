81891c8a0d7662e1cf5c78d2e99d7b62e499f14e
dawid gostyński <dawid.gostynski@bluesoft.com>
2022-12-01 13:18:23 +0100

    Parametrized talend cloud environment

--- createEng.py
+++ createEng.py
@@ -86,9 +86,9 @@ def createRemoteEngine(vmDistribution = 'ubuntu', enableDebugInStudio = True, na
 try:
     vmDistribution = sys.argv[1]
     enableDebugInStudio = sys.argv[2]
+    env = sys.argv[3]
     pToken = "Bearer wHiwDdZWTTasSdvCFIxNmwcSXglP2ozKgnU7qdpKXP1-Sh1z3ygwyJcNkVF6z4H5"
     name="testName"
-    env = "R&D"
     key, engineName = createRemoteEngine(vmDistribution=vmDistribution, enableDebugInStudio=enableDebugInStudio, name=name, env=env, personalToken = pToken)
     with open('/home/python/preauthorized.key.cfg', 'w') as f:
         f.write(key)
